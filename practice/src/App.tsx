79cd84f22aedb620e33b5f714964880248789fe9
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2022-11-21 17:32:33 +0700

    implement component text

--- practice/src/App.tsx
+++ practice/src/App.tsx
@@ -1,7 +1,32 @@
-function App():JSX.Element {
+import { FONT_SIZE_400, FONT_SIZE_700, FONT_WEIGHT } from './types';
+
+import { Text } from './components/text';
+
+function App(): JSX.Element {
     return (
-      <div className="App"></div>
-    )
+        <div className="App">
+            <Text
+                type="p"
+                size={{
+                    fontSize: FONT_SIZE_400.FONT_SIZE_16,
+                    fontWeight: FONT_WEIGHT.FONT_WEIGHT_400,
+                }}
+            >
+                Hello
+            </Text>
+
+            <Text type="p">Hello</Text>
+            <Text
+                type="p"
+                size={{
+                    fontSize: FONT_SIZE_700.FONT_SIZE_19,
+                    fontWeight: FONT_WEIGHT.FONT_WEIGHT_700,
+                }}
+            >
+                Hello 1
+            </Text>
+        </div>
+    );
 }
 
 export default App;
