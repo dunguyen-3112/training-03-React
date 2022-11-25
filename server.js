0bba647abe7a529f1eeb6c5de49352c4e8364586
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2022-11-25 22:30:11 +0700

    config deploy heroku

--- server.js
+++ server.js
@@ -1,85 +1,275 @@
-const fs = require("fs");
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("data.json");
-const middlewares = jsonServer.defaults();
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const jsonServer = require("json-server");
 
-const userdb = JSON.parse(fs.readFileSync("data.json", "UTF-8"));
+const db = require("./db.json");
+const fileName = "./db.json";
 
-const privateKey = "31122001";
+const PORT = process.env.PORT || 3000;
 
-console.log(userdb);
+const server = jsonServer.create();
+const router = jsonServer.router(fileName);
+const middlewares = jsonServer.defaults();
+
+let refreshTokens = [];
 
 server.use(middlewares);
 
-server.get("/echo", (req, res) => {
-    res.jsonp(req.query);
-});
+server.use(
+    jsonServer.rewriter({
+        "/api/users": "/users",
+    })
+);
 
 server.use(jsonServer.bodyParser);
-server.use((req, res, next) => {
-    const url = req.url.split("/")[1];
-    if (req.method === "POST") {
-        switch (url) {
-            case "signup":
-                console.log("check: " + url);
-                break;
-            case "login":
-                console.log(req.body);
-                res.json({ id: "1234" });
-                break;
-            case "checkLogin":
-                console.log(req.body);
-                res.json({ id: "1234" });
-                break;
 
-            default:
-                break;
+server.use((req, res, next) => {
+    try {
+        const urls = req.path.split("/");
+
+        const url = urls[1];
+
+        if (url == "login") handleLogin(req, res);
+        // refresh token
+        else if (url == "token") {
+            const { token } = req.body;
+
+            if (!refreshTokens.includes(token)) return res.sendStatus(403);
+
+            jwt.verify(
+                token,
+                process.env.REFRESH_TOKEN_SECRET,
+                (err, { id, role }) => {
+                    if (err) return res.sendStatus(403);
+                    const access_token = generateAccessToken({
+                        id,
+                        role,
+                    });
+                    res.json({
+                        access_token,
+                    });
+                }
+            );
+        }
+        //
+        else if (
+            req.method == "GET" &&
+            (url === "statuses" || url === "priorities")
+        )
+            authenticateToken(req, res) == true && next();
+        else {
+            authenticateToken(req, res) == true &&
+                navigationRole(req, res, next);
+        }
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+server.use(router);
+
+server.listen(PORT, () => {
+    console.log("JSON Server is running on port " + PORT);
+});
+
+function handleLogin(req, res) {
+    const { email, password } = req.body;
+    user = db.users.find((user) => user.email === email);
+
+    if (user?.password === password) {
+        const access_token = generateAccessToken({
+            id: user.id,
+            role: user.role,
+        });
+        const refresh_token = jwt.sign(
+            {
+                id: user.id,
+                role: user.role,
+            },
+            process.env.REFRESH_TOKEN_SECRET
+        );
+        refreshTokens.push(refresh_token);
+
+        res.json({
+            status: 200,
+            message: "Login successful!",
+            access_token,
+            refresh_token,
+        });
+    } else
+        res.json({
+            status: 401,
+            message: "Login failed! You please check email or password ?",
+        });
+}
+function authenticateToken(req, res) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, { id, role }) => {
+        if (err) return res.sendStatus(403);
+        req.userId = id;
+        req.userRole = role;
+    });
+    return true;
+}
+
+function navigationRole(req, res, next) {
+    const role = req.userRole;
+
+    const url = req.path.split("/")[1];
+    const method = req.method;
+
+    let obj;
+
+    let id = req.query.id || req.path.split("/")[2];
+
+    const date_regex = /^\d{4}\/\d{2}\/\d{2}$/;
+
+    if (url === "tickets" && (method === "PUT" || method === "POST")) {
+        const status = req.body.status;
+        const priority = req.body.priority;
+        const name = req.body.name;
+        const description = req.body.description;
+        const assignBy = req.body.assignBy;
+        const dueDate = req.body.dueDate;
+
+        if (
+            typeof status !== "number" ||
+            status > 4 ||
+            status < 0 ||
+            typeof priority !== "number" ||
+            priority < 0 ||
+            priority > 3 ||
+            name === undefined ||
+            typeof name !== "string" ||
+            name.trim().length <= 0 ||
+            description === undefined ||
+            typeof description !== "string" ||
+            description.trim().length <= 0 ||
+            assignBy === undefined ||
+            typeof assignBy !== "string" ||
+            assignBy.trim().length <= 0 ||
+            dueDate === undefined ||
+            typeof dueDate !== "string" ||
+            dueDate.trim().length <= 0 ||
+            !date_regex.test(dueDate) ||
+            new Date(dueDate).toString() === "Invalid Date" ||
+            new Date(dueDate).getTime() < new Date().getTime()
+        )
+            return res.sendStatus(400);
+
+        if (method === "PUT") {
+            obj = db.tickets.find((ticket) => ticket.id === id);
+
+            if (
+                JSON.stringify(obj) ===
+                JSON.stringify({
+                    ...req.body,
+                    id,
+                    createDate: obj.createDate,
+                    createBy: obj.createBy,
+                })
+            ) {
+                return res.sendStatus(202);
+            }
+
+            if (
+                new Date(dueDate).toString() === "Invalid Date" ||
+                new Date(dueDate).getTime() < new Date(obj.createDate).getTime()
+            )
+                return res.sendStatus(400);
+
+            if (obj == null) return res.sendStatus(400);
+
+            Object.assign(req.body, {
+                id,
+                ...obj,
+                ...req.body,
+            });
+        } else if (method === "POST") {
+            const date = new Date();
+            Object.assign(req.body, {
+                ...req.body,
+                createDate: `${date.getDate()}/${date.getMonth() + 1}/${
+                    date.getFullYear() + 1
+                }`,
+                createBy: req.userId,
+            });
         }
     }
-    next();
-});
 
-// server.use(
-//   jsonServer.rewriter({
-//     "api/v1/getusers?id=:uid": "/api/users/:uid",
-//   })
-// );
-
-// Use default router
-server.use("/api", router);
-server.listen(3000, () => {
-    console.log("JSON Server is running");
-});
-
-router.render = (req, res) => {
-    const url = req.url.split("/")[1];
-
-    let data;
-
-    console.log("render: " + url);
-
-    switch (url) {
-        case "users":
-            const { firstName, lastName, avatarUrl } = res.locals.data;
-            data = { firstName, lastName, avatarUrl };
+    switch (role) {
+        case "manager":
+            next();
             break;
-        case "signup":
-            const { email, password } = res.locals.data;
-            const access_token = jwt.sign(
-                {
-                    email,
-                    password,
-                    exp: Math.floor(Date.now() / 1000) + 60 * 60,
-                },
-                privateKey
-            );
-            data = { access_token, email };
-            console.log(data);
+        case "developer":
+            switch (url) {
+                case "tickets":
+                    switch (method) {
+                        case "GET":
+                            let resObj;
+
+                            resObj = id
+                                ? db.tickets.find((ticket) => ticket.id === id)
+                                : db.tickets.filter(
+                                      (ticket) => ticket.createBy === req.userId
+                                  );
+
+                            if (id && resObj.createBy !== req.userId)
+                                return res.sendStatus(403);
+
+                            return resObj == null || resObj == []
+                                ? res.sendStatus(404)
+                                : res.json({
+                                      body: resObj,
+                                      status: 200,
+                                      message: "",
+                                  });
+
+                        case "POST":
+                            next();
+                            break;
+
+                        case "DELETE":
+                            const ticket = db.tickets.find(
+                                (ticket1) => ticket1.id === id
+                            );
+
+                            if (ticket == null) return res.sendStatus(400);
+                            if (ticket.createBy !== req.userId)
+                                return res.sendStatus(403);
+                            next();
+                            break;
+                        case "PUT":
+                            if (req.body.createBy !== req.userId)
+                                return res.sendStatus(403);
+                            next();
+                            break;
+                        default:
+                            res.sendStatus(404);
+                            break;
+                    }
+                    break;
+
+                default:
+                    res.send(404);
+                    break;
+            }
             break;
         default:
+            res.sendStatus(404);
             break;
     }
-    res.jsonp({ data });
-};
+}
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15s",
+    });
+}
+
+function validTicket(obj) {}
