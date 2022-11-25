1949c9a4e4c1fec349f00e15a0c165e0840b4f6b
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2022-11-25 14:50:04 +0700

    merege to practice_one branch

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { Button } from './components/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Button text="Sowf vaos" />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -17,18 +19,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ my-react-app/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Button extends React.Component {
+    render() {
+        return <Button onPress={{}}>{this.props.text}</Button>
+    }
+}
+
+export { Button }
