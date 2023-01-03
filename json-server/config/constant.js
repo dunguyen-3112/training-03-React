3c386630e7fe5fde0d6f0e171ec1da3a158c9226
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2023-01-03 09:19:22 +0700

    clear

--- json-server/config/constant.js
+++
@@ -1,7 +0,0 @@
-const priorities = ['High', 'Low', 'Normal'];
-const statuses = ['Todo', 'In-progress', 'Review', 'Done'];
-
-const itemsPerPage = 10;
-
-// eslint-disable-next-line no-undef
-module.exports = { priorities, statuses, itemsPerPage };
--- json-server/model/utils.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-const fs = require('fs');
-
-function saveChanges(json) {
-    fs.writeFileSync('db.json', JSON.stringify(json));
-}
-
-module.exports = saveChanges;
