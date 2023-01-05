9ac726b40e1f8cdecc8063a12e103c947789c4ea
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2023-01-05 22:42:03 +0700

    define constants

---
+++ app/src/constants/errors.js
@@ -0,0 +1,2 @@
+export const ERROR_REQUIRED_FIELD = "This field is required";
+export const ERROR_INVALID_EMAIL = "Please enter a valid email address";
---
+++ app/src/constants/routes.js
@@ -0,0 +1,3 @@
+export const HOME_ROUTE = "/";
+export const ABOUT_ROUTE = "/tickets";
+export const CONTACT_ROUTE = "/login";
