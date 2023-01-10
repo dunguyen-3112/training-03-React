a1e77f5e90fcbb2c8dd92d77683283b35eee7b1e
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2023-01-10 16:05:47 +0700

    Update Ticket Row component

--- app/src/pages/Home/TicketRow/TicketRow.jsx
+++ app/src/pages/Home/TicketRow/TicketRow.jsx
@@ -26,7 +26,6 @@ const TicketRow = ({ ticket, onEdit }) => {
     const url =
         ticket.customerAvatar ||
         "https://t4.ftcdn.net/jpg/03/59/58/91/360_F_359589186_JDLl8dIWoBNf1iqEkHxhUeeOulx0wOC5.jpg";
-
     return (
         <tr
             key={ticket.id}
