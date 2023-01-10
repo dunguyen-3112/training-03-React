ed303a75a1eb5b93b6c03e4868111fa6aed7b433
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2023-01-10 16:02:28 +0700

    Handle get user By id BE

--- json-server/model/User.js
+++ json-server/model/User.js
@@ -20,8 +20,23 @@ class User {
    * @returns User
    */
   static async findUserById(id) {
-    console.log(id);
     return await db.users.find((user) => user.id === id);
+  }
+
+  /**
+   *
+   * @param {string} query
+   * @returns
+   */
+  static async searchUser(query) {
+    query = query.toLowerCase();
+    const users = await db.users.filter(
+      (user) =>
+        user.lastName.toLowerCase().includes(query) ||
+        user.firstName.toLowerCase().includes(query)
+    );
+    console.log(users);
+    return users;
   }
 }
 
