99eb9cb7518bb6ebbe40969702a6980c9609e2d4
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2023-01-13 17:07:06 +0700

    Update main Layout

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,53 +1,45 @@
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { React, useState, useEffect } from "react";
 
-import "./App.css";
 import ContextProvider from "@context/ContextProvider";
-import Navbar from "@layouts/Navbar";
+import Header from "@layouts/Header";
 import SideBar from "@layouts/Sidebar";
-
+import * as API from "@utils/api";
 import { routes } from "@routes";
-import LoginPage from "@src/pages/LoginPage";
-import { HandleLogout } from "@services/auth";
+import { LOGIN_ROUTE, ME_ROUTE, OK, TICKET_ROUTE } from "./constants";
 
 function App() {
-  const [isAuthentication, setIsAuthentication] = useState(false);
-  const index = routes.findIndex(
-    (route) => route.icon && route.path.includes(location.href.split("/").at(3))
-  );
-  const [page, setPage] = useState(index);
+  console.log("APP");
+  const [page, setPage] = useState(0);
+  const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const refreshToken = localStorage.getItem("refresh_token");
-    refreshToken && setIsAuthentication(true);
-  }, [navigate, isAuthentication]);
-
-  const handleLogin = () => {
-    setIsAuthentication(true);
-  };
-
-  const handleLogout = async () => {
-    const response = await HandleLogout();
-    console.log(response);
-    localStorage.clear();
-    setIsAuthentication(false);
-  };
-
-  if (!isAuthentication) return <LoginPage onLogin={handleLogin} />;
+    async function refreshLogin() {
+      const response = await API.get(`/${ME_ROUTE}`);
+      if (response?.status === OK) {
+        const user = response.data;
+        setUser(user);
+      } else navigate(`/${LOGIN_ROUTE}`);
+    }
+    user === null ? refreshLogin() : navigate(`/${TICKET_ROUTE}`);
+  }, [user]);
 
   return (
-    <ContextProvider value={{ isLogin: isAuthentication, page, setPage }}>
-      <main className="main-container">
-        <SideBar />
-        <div className="main-section">
-          <Navbar handleLogout={handleLogout} />
-          <Routes>
-            {routes.map((route) => (
-              <Route key={route.path} {...route} />
-            ))}
-          </Routes>
-        </div>
+    <ContextProvider value={{ page, setPage, user, setUser }}>
+      <main className="flex" data-authenticated="true">
+        {user && (
+          <>
+            <SideBar />
+            <Header />
+          </>
+        )}
+
+        <Routes>
+          {routes.map((route) => (
+            <Route key={route.path} {...route} />
+          ))}
+        </Routes>
       </main>
     </ContextProvider>
   );
